abddf4a1f1973729e2cd3b231ebdef206bc19fcc
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2021-01-23 15:21:25 -0600

    SideBar
    Barra de menú

--- teca/src/app/shared/shared.module.ts
+++ teca/src/app/shared/shared.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { IonicModule } from '@ionic/angular';
+import { ProfessorSideBarComponent } from './components/side-bar/professor-side-bar/professor-side-bar.component';
+import { StudentSideBarComponent } from './components/side-bar/student-side-bar/student-side-bar.component';
+
 
 
 
 @NgModule({
-  declarations: [BannerComponent],
-  exports:[BannerComponent],
+  declarations: [
+      BannerComponent
+    , ProfessorSideBarComponent
+    , StudentSideBarComponent
+    , ProfessorSideBarComponent],
+  exports:[
+      BannerComponent
+    , ProfessorSideBarComponent
+    , StudentSideBarComponent
+    , ProfessorSideBarComponent],
   imports: [
     CommonModule,
     IonicModule
