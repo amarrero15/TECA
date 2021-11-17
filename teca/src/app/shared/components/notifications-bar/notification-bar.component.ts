4c2c8ce4707813cdd67fb7cd161e1fad3f2260ca
unknown <mariangelanavrro10@gmail.com>
2021-11-16 19:43:07 -0600

    x

---
+++ teca/src/app/shared/components/notifications-bar/notification-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-NotificationBar',
+  templateUrl: './notification-bar.component.html',
+  styleUrls: ['./notification-bar.component.scss'],
+})
+export class NotificationBar implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
